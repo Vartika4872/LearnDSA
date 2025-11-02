e3068fe092013d1b89304a07dbc364974585ef72
Vartika Singh <vartikasinghv37@gmail.com>
2025-11-02 14:21:14 +0000

    Matrix

---
+++ SetMatrixZeroInAnArray.java
@@ -0,0 +1,50 @@
+class SetMatrixZeroInAnArray {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 0, 6},
+            {7, 8, 9}
+        };
+
+        setZeros(matrix);
+
+        // Print the updated matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void setZeros(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[] row = new int[n];
+        int[] col = new int[m];
+
+        // Mark rows and columns that need to be zeroed
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Set cells to zero where needed
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+
+
